fb0eaef63711e7a0091f583bc3ace917a2df956a
mr-cheeezz <skylerspear26@gmail.com>
2023-05-16 17:24:52 -0400

    first commit

---
+++ system/cpu.go
@@ -0,0 +1,18 @@
+package system
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func Cpu() string {
+	cmd := exec.Command("wmic", "cpu", "get", "Name")
+	output, err := cmd.Output()
+	if err == nil {
+		lines := strings.Split(string(output), "\n")
+		if len(lines) >= 2 {
+			cpuInfo := strings.TrimSpace(lines[1])
+			return cpuInfo
+		}
+	}
+}
